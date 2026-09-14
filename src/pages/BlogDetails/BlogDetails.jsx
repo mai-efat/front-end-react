e6df2c0605bb401d69df8f119db4d12ef8375e6b
Muhammed Amer <mhmedamer72@gmail.com>
2026-09-14 14:54:36 +0300

    Blog details page created plus updating vars inside root of the PR

--- src/pages/BlogDetails/BlogDetails.jsx
+++ src/pages/BlogDetails/BlogDetails.jsx
@@ -1,5 +1,14 @@
-import Navbar from '../../components/Navbar/Navbar';
-import Footer from '../../components/Footer/Footer';
+import styles from './BlogDetails.module.css';
+import ArticleHeader from './Components/articleHeader/ArticleHeader';
+import ArticleContent from './Components/articleContent/ArticleContent';
+import ArticleSidebar from './Components/articleSidebar/ArticleSidebar';
+import CtaCard from './Components/ctaCard/CtaCard';
+import MoreArticles from './Components/moreArticles/MoreArticles';
+
+
+
+
+
 
 
 
@@ -7,9 +16,25 @@ function BlogDetails() {
          
   return (
              <>
-        <Navbar/>
-  <h1>welcome to blog details</h1>
-          <Footer/>
+      
+    <main className={styles.blogContent}>
+      <section className= {styles.articleLayout}>
+         <article className= {styles.articleContent}>
+            <ArticleHeader/>
+           <ArticleContent/>
+           <CtaCard/>
+         </article>
+         <aside >
+        <ArticleSidebar/>
+        </aside>
+
+      </section>
+      
+      <section className={styles.moreSynkra}>
+       <MoreArticles/>
+      </section>
+    </main>
+         
              </>
 
    );
